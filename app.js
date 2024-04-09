0d3bb25b4855014de1880ba5b8a6a8d33ddcc7f5
Vrund Patel <vrundp1308@gmail.com>
2024-04-09 14:22:41 -0400

    installed dependencies, added gitignore, added basic stub to app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+import configRoutesFunction from "./routes/index.js";
+
+configRoutesFunction(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});

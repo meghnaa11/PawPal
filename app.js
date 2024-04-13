e0cce249adf3d4aab73627c92bc915864db4a13a
Yichao Jiang <jychwd@gmail.com>
2024-04-13 01:32:13 -0400

    updated the app.js midddleware

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 import express from "express";
 const app = express();
 import configRoutesFunction from "./routes/index.js";
+import exphbs from 'express-handlebars';
+import session from 'express-session';
+
+// const rewriteUnsupportedBrowserMethods = (req, res, next) => {
+//   // If the user posts to the server with a property called _method, rewrite the request's method
+//   // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
+//   // rewritten in this middleware to a PUT route
+//   if (req.body && req.body._method) {
+//     req.method = req.body._method;
+//     delete req.body._method;
+//   }
+
+//   // let the next middleware run:
+//   next();
+// };
+app.use(
+  session({
+    secret: "key",
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: 60000 }
+  })
+);
+app.use('/public', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(rewriteUnsupportedBrowserMethods);
+
+app.engine('handlebars', exphbs.engine({
+  defaultLayout: 'main',
+  partialsDir: ['views/partials/']
+}));
+app.set('view engine', 'handlebars');
+
 
 configRoutesFunction(app);
 

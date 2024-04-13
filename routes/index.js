cb0533c3848df3b3adb95303f6e36c178f3061d0
Yichao Jiang <jychwd@gmail.com>
2024-04-13 19:50:19 -0400

    updated the index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,23 @@
-// This file will import both route files and export the constructor method as shown in the lecture code
+import institutionRoutes from './institutions.js';
+import appointmentRoutes from './appointments.js';
 
-/*
-    - When the route is /products use the routes defined in the products.js routing file
-    - When the route is /reviews use the routes defined in reviews.js routing file
-    - All other enpoints should respond with a 404 as shown in the lecture code
-*/
 
-import productRoutes from "./products.js";
-import reviewRoutes from "./reviews.js";
 
 const constructorMethod = (app) => {
-  app.use("/products", productRoutes);
-  app.use("/reviews", reviewRoutes);
 
-  app.use("*", (req, res) => {
-    res.status(404).json({ error: "Not found" });
+
+
+  app.use('/institution', institutionRoutes);
+  app.use('/getapp', appointmentRoutes);
+
+  app.get("/", (req, res) => {
+    res.render('home/index', {});
+
   });
+
+  // app.use("*", (req, res) => {
+  //   res.status(404).json({ error: "Not found" });
+  // });
 };
 
 export default constructorMethod;
